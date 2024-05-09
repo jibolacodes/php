e2fe9b9ecda90d954cd8286232ca05fd0e13fe38
jibolacodes <jibzsupreme@gmail.com>
2024-05-09 14:09:42 +0100

    calculator using if statements

--- site.php
+++ site.php
@@ -8,40 +8,29 @@
 </head>
 
 <body>
+  <!-- Calculator -->
   <form action="site.php" method="get">
-    <input type="text" name="firstColor">
-    <br>
-    <input type="text" name="secondColor">
-    <br>
-    <input type="text" name="person">
-    <br>
+    Number 1: <input type="number" step="0.1" name="firstNum" /><br>
+    Operator: <input type="text" name="operator" /><br>
+    Number 1: <input type="number" name="secondNum" />
     <input type="submit">
   </form>
-
   <?php
-  $firstColor = $_GET["firstColor"];
-  $secondColor = $_GET["secondColor"];
-  $person = $_GET["person"];
+  $num1 = $_GET["firstNum"];
+  $num2 = $_GET["secondNum"];
+  $op = $_GET["operator"];
 
-  echo "
-  <article>
-    <p>Roses are $firstColor </p>
-    <p>Violets are $secondColor </p>
-    <p>I love $person </p>
-  </article>
-  "
-  ?>
-
-  <form action="site.php" method="post">
-    Apples : <input type="checkbox" name="fruits[]" value="apples"><br>
-    Oranges : <input type="checkbox" name="fruits[]" value="oranges"><br>
-    Pears : <input type="checkbox" name="fruits[]" value="pears"><br>
-    <input type="submit">
-  </form>
-
-  <?php
-  $fruits = $_POST["fruits"];
-  echo $fruits[2];
+  if ($op == "+") {
+    echo $num1 + $num2;
+  } elseif ($op == "-") {
+    echo $num1 - $num2;
+  } elseif ($op == "/") {
+    echo $num1 / $num2;
+  } elseif ($op == "*") {
+    echo $num1 * $num2;
+  } else {
+    echo "Invalid Operator";
+  }
   ?>
 </body>
 
